5770109c1a06962f5915a9b2460700b93bd61f7a
Oliver Hader <oliver@typo3.org>
2018-01-07 19:51:08 +0100

    [TASK] Add basic PhpDriver acceptance test

---
+++ tests/acceptance/tests/acceptance/CarRentalCest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class CarRentalCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('Show the home page');
+        $I->amOnPage('/');
+        $I->see('Welcome!', '#content .container header > h2');
+
+        $navBarSelector = 'body > div.body-bg > header > div > nav';
+        $I->see('Home', $navBarSelector);
+        $I->see('Products', $navBarSelector);
+        $I->see('About us', $navBarSelector);
+        $I->see('Login', $navBarSelector);
+        $I->click('Products', $navBarSelector);
+        $I->see('Rent a Car');
+    }
+}
